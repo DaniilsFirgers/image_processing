b53d738e226b5501e1f2617ed54da67fdbe973a9
Daniils Firgers <dfirger@gmail.com>
2023-09-14 17:49:35 +0300

    finished histogram equalization

--- histogram_equalization/main.py
+++ histogram_equalization/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-from utils import image_to_array
+from utils import image_to_array, get_intensity_frequency, get_cumulative_distribution, get_mapped_intensity_levels
 
 unequalized_image = Image.open("/app/images/Unequalized_Hawkes_Bay_NZ.jpg")
 
@@ -8,6 +8,15 @@ unequalized_image_grayscale = unequalized_image.convert("L")
 
 unequalized_image_array = image_to_array(unequalized_image_grayscale)
 
-print(unequalized_image_array)
+unequalized_image_frequency = get_intensity_frequency(unequalized_image_array)
 
-print("Hello world!")
+cumulative_distribution = get_cumulative_distribution(
+    unequalized_image_frequency)
+
+mapped_intensity_levels = get_mapped_intensity_levels(cumulative_distribution)
+equalized_image_array = [[mapped_intensity_levels[pixel]
+                          for pixel in row] for row in unequalized_image_array]
+
+equalized_image_array = np.array(equalized_image_array, dtype=np.uint8)
+eq_img = Image.fromarray(equalized_image_array, mode='L')
+eq_img.save("/app/images/Equalized_Hawkes_Bay_NZ.jpg")
--- histogram_equalization/utils.py
+++ histogram_equalization/utils.py
@@ -3,8 +3,37 @@ def image_to_array(image):
     pixel_values = []
 
     for y in range(height):
+        row = []
         for x in range(width):
             pixel_value = image.getpixel((x, y))
-            pixel_values.append(pixel_value)
+            row.append(pixel_value)
+        pixel_values.append(row)
 
     return pixel_values
+
+
+def get_intensity_frequency(image_array):
+    frequency_counts = [0] * 256
+    for row in image_array:
+        for intensity in row:
+            frequency_counts[intensity] += 1
+    return frequency_counts
+
+
+def get_cumulative_distribution(frequency_counts: list[int]):
+    cdf = [0] * 256
+    for i in range(256):
+        if i == 0:
+            cdf[i] = frequency_counts[i]
+        else:
+            cdf[i] = cdf[i - 1] + frequency_counts[i]
+
+    total_pixels = sum(frequency_counts)
+    normalized_cdf = [cdf[i] / total_pixels for i in range(256)]
+    return normalized_cdf
+
+
+def get_mapped_intensity_levels(normalized_cdf: list[float]):
+    mapped_intesity_levels = [round(cdf_value * 255)
+                              for cdf_value in normalized_cdf]
+    return mapped_intesity_levels
