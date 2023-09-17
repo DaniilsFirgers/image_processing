eae1ed8d3dc77afd5cd75564a2e36d60ec0c80f9
Daniils Firgers <dfirger@gmail.com>
2023-09-17 20:29:58 +0300

    add apply_gaussian_filter to blur utils

--- gaussian_blurring/main.py
+++ gaussian_blurring/main.py
@@ -1,7 +1,17 @@
-from utils import generate_gaussian_kernel
+from utils import generate_gaussian_kernel, image_to_array, apply_gaussian_blur
+from PIL import Image
 
 KERNEL_SIZE = 3
 SIGMA = 0.6
 
+
+unbluured_image = Image.open("/app/images/cat.jpg")
+
+converted_unblurred_image = unbluured_image.convert("L")
+
+unblurred_image_array = image_to_array(converted_unblurred_image)
+
 kernel = generate_gaussian_kernel(KERNEL_SIZE, SIGMA)
-print(kernel)
+
+blurred_image_array = apply_gaussian_blur(unblurred_image_array, kernel)
+print(blurred_image_array)
--- gaussian_blurring/utils.py
+++ gaussian_blurring/utils.py
@@ -1,4 +1,5 @@
 import math
+from PIL import Image
 
 
 def generate_gaussian_kernel(size: int, sigma: float) -> list[list[float]]:
@@ -20,3 +21,39 @@ def generate_gaussian_kernel(size: int, sigma: float) -> list[list[float]]:
             total += kernel[i][j]
 
     return [[element / total for element in row] for row in kernel]
+
+
+def apply_gaussian_blur(image: Image, kernel: list[list[float]]) -> list[list[int]]:
+    """
+    Applies a Gaussian blur to an image
+    """
+    result = [[0 for _ in range(len(image[0]))] for _ in range(len(image))]
+    k_size = len(kernel) // 2
+
+    for i in range(k_size, len(image) - k_size):
+        for j in range(k_size, len(image[0]) - k_size):
+            pixel_sum = 0
+            for x in range(len(kernel)):
+                for y in range(len(kernel[0])):
+                    pixel_sum += image[i + x - k_size][j +
+                                                       y - k_size] * kernel[x][y]
+            result[i][j] = int(pixel_sum)
+
+    return result
+
+
+def image_to_array(image):
+    """
+    Converts a PIL image to a 2D array
+    """
+    width, height = image.size
+    pixel_values = []
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            pixel_value = image.getpixel((x, y))
+            row.append(pixel_value)
+        pixel_values.append(row)
+
+    return pixel_values
