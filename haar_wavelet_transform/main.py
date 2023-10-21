f5339cd412b1f8c36a7a6110d7dea9d4abe283da
Daniils Firgers <dfirger@gmail.com>
2023-10-21 22:54:56 +0300

    finish haar

--- haar_wavelet_transform/main.py
+++ haar_wavelet_transform/main.py
@@ -1,31 +1,73 @@
-def haarFWT(signal, level):
+def haar(signal, level):
 
-    s = .5                  # scaling -- try 1 or ( .5 ** .5 )
+    s = .5
 
-    h = [1,  1]           # lowpass filter
-    g = [1, -1]           # highpass filter
-    f = len(h)           # length of the filter
+    # Both low and high filters help to separate different frequency components of the signal
+    # We will aplly filter to all adjacent values in the signal
 
-    t = signal              # 'workspace' array
-    l = len(t)           # length of the current signal
-    y = [0] * l             # initialise output
+    # Used to extract the low-frequency components or approximation coefficients
+    # Each value in the original signal is multiplied by 1
+    # Extracts LL subband
+    low_pass_f = [1,  1]
 
-    t = t + [0, 0]        # padding for the workspace
+    # Used to extract high-frequency components or detail coefficients
+    # First value is multiplied by 1, second by -1
+    # Extracts LH, HL, HH subbands
+    high_pass_f = [1, -1]
 
+    # Length of the filter
+    # We need it to know how many adjacent values we need to multiply by the filter
+    filter_len = len(low_pass_f)
+
+    # Create a copy of the signal
+    signal_copy = signal
+
+    # Length of the signal
+    signal_len = len(signal_copy)
+
+    # Initialise the array for the output (all zeros)
+    output = [0] * signal_len
+
+    # We extend the the orginal signal by two zeros
+    # Now sequence is of power of 2, which is a good tehcnique for the wavelet transform
+    signal_copy = signal_copy + [0, 0]
+
+    # We will apply the filter to the signal level times
     for i in range(level):
 
-        y[0:l] = [0] * l  # initialise the next level
-        l2 = l // 2         # half approximation, half detail
+        # We initialize the output array with zeros (length of the signal)
+        output[0:signal_len] = [0] * signal_len
+        # We divide the signal into two parts
 
-        for j in range(l2):
-            for k in range(f):
-                y[j] += t[2*j + k] * h[k] * s
-                y[j+l2] += t[2*j + k] * g[k] * s
+        # First part is approximation (low frequency components), second is detail ()
+        # Represents the length of the approximation coefficients at the current level
+        split_len = signal_len // 2
 
-        l = l2              # continue with the approximation
-        t[0:l] = y[0:l]
+        # Iterate over split signal
+        for j in range(split_len):
+            # Iterate over filter
+            for k in range(filter_len):
+                # Update j element of the output array.
+                # Signal_copy[2*j + k] refers to the original signal value.
+                # 2*j is used to ensure we're working with adjacent pairs of values in the original signal
+                # k is used to access the appropriate filter coefficient.
+                # low_pass_f[k] represents the k-th coefficient of the low-pass filter
+                output[j] += signal_copy[2*j + k] * low_pass_f[k] * s
 
-    return y
+                # Updates the j + split_len-th element of the output array
+                # high_pass_f[k] represents the k-th coefficient of the high-pass filter.
+                output[j+split_len] += signal_copy[2*j + k] * \
+                    high_pass_f[k] * s
+
+        # updates the length of the signal (signal_len) to be equal to split_len
+        signal_len = split_len
+
+        # This line copies the content of output back into signal_copy. The copy is limited to the first signal_len elements
+        # Signal_copy is a copy of the original signal. After each iteration of the transform, it's updated to contain the transformed coefficients
+        # Output contains the newly computed coefficients for the current level
+        signal_copy[0:signal_len] = output[0:signal_len]
+
+    return output
 
 
 def main():
@@ -35,13 +77,13 @@ def main():
     print(s0)
 
     print("level 1")
-    print(haarFWT(s0, 1))
+    print(haar(s0, 1))
 
     print("level 2")
-    print(haarFWT(s0, 2))
+    print(haar(s0, 2))
 
     print("level 3")
-    print(haarFWT(s0, 3))
+    print(haar(s0, 3))
 
 
 if __name__ == "__main__":
