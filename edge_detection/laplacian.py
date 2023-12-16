f040d6b943ac6a356a75e0b537959786472b3d97
Daniils Firgers <dfirger@gmail.com>
2023-12-16 21:53:15 +0200

    add comments

--- edge_detection/laplacian.py
+++ edge_detection/laplacian.py
@@ -2,44 +2,49 @@ from PIL import Image
 
 
 def laplacian_filter(image):
-    # Convert the image to grayscale
+    # Convert the image to grayscale using the Luma formula
     grayscale_image = image.convert("L")
 
-    # Laplacian filter kernel
+    # Laplacian 3x3 filter kernel which is applied to every pixel of the image
+    # The center pixel has the value 8, the surrounding pixels have the value -1, which are weights
+    # 8 emphasizes high frequency changes in intensity, -1 emphasizes rapid changes in intensity
     kernel = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
 
-    # Get image size
+    # Get image width and height
     width, height = grayscale_image.size
 
-    # Create a new image to store the result
+    # Create a new image of the same width and height to store the converted image
     result_image = Image.new("L", (width, height))
 
+    # Apply the Laplacian filter to each pixel of of vertical and horizontal neighbors
+    # -1 means that the pixel is on the edge of the image and has no neighbor, so it is ignored
     for y in range(1, height - 1):
         for x in range(1, width - 1):
-            # Calculate the Laplacian filter value
+            # For each pixel thhe value is calculated by summing the product of the pixel values
+            # and corresponding kernel values within the 3x3 neighborhood
+            # grayscale_image.getpixel((x + i - 1, y + j - 1)) retreives the pixel intensity value at a coordinate
+            # kernel[i][j] retreives the corresponding kernel value
+            # for i in range(3) for j in range(3) - this is a nested loop that iterates over the 3x3 Laplacian kernel
+            # overall it computes the sum of the weighted pixel values, representing the convolution operation
             laplacian_value = sum(grayscale_image.getpixel(
                 (x + i - 1, y + j - 1)) * kernel[i][j] for i in range(3) for j in range(3))
 
-            # Ensure the value is in the valid range [0, 255]
+            # Then value is scaled to the range [0, 255]
             laplacian_value = max(0, min(255, laplacian_value))
 
-            # Set the pixel in the result image
+            # Then we set the pixel in the result image
             result_image.putpixel((x, y), laplacian_value)
 
     return result_image
 
 
 def main():
-    # Open the image using PIL
     input_image_path = "images/original_image.png"
     output_image_path = "images/laplacian.png"
 
     original_image = Image.open(input_image_path)
-
-    # Apply the Laplacian filter
     laplacian_image = laplacian_filter(original_image)
 
-    # Save the result
     laplacian_image.save(output_image_path)
     print(f"Laplacian filter applied. Result saved at: {output_image_path}")
 
