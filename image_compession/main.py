f52cfa0426ec956e836cbe5d28eaf06a7d71c9c0
Daniils Firgers <dfirger@gmail.com>
2023-12-23 23:18:30 +0200

    finish image compression

--- image_compession/main.py
+++ image_compession/main.py
@@ -3,22 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def dpcm_encode(image):
+def dpcm_encode(image, compression_strength):
+    # Get the height and width of the image
     height, width = image.shape
+    # Initialize the encoded values list
     encoded = []
 
     # Initialize the predictor with the first pixel value
+    # used to estimate tthe current sample based on the previous one
     predictor = image[0, 0]
 
+    # Iterate over the image's rows and columns
     for row in range(height):
         for col in range(width):
-            # Calculate the prediction error
+            # Calculate the prediction error by subtracting the predictor from the actual pixel value
+            # Represents how much the current prediction differs from the actual pixel value
             prediction_error = image[row, col] - predictor
 
-            # Quantize the prediction error (in this case, using a simple threshold)
-            quantized_error = 1 if prediction_error >= 0 else -1
+            # Quantize the prediction error based on compression_strength
+            # Reduces the precision of the prediction error
+            # The quanitization aims to reduce the number of bits required to store the error
+            quantized_error = int(
+                round(np.clip(prediction_error / compression_strength, -255, 255)))
 
-            # Update the predictor for the next pixel
+            # Update the predictor for the next sample in the sample
             predictor += quantized_error
 
             # Store the quantized error
@@ -27,39 +35,22 @@ def dpcm_encode(image):
     return encoded
 
 
-def dpcm_decode(encoded, original_shape):
-    height, width = original_shape
-    decoded = np.zeros((height, width), dtype=np.uint8)
+def save_as_compressed_image(encoded_values, image_shape, compression_strength):
+    encoded_array = np.array(encoded_values).reshape(image_shape)
 
-    # Initialize the predictor with the first pixel value
-    predictor = 0
+    compressed_image = Image.fromarray(encoded_array.astype(np.uint8))
 
-    for row in range(height):
-        for col in range(width):
-            # Retrieve the quantized error
-            quantized_error = encoded[row * width + col]
-
-            # Update the predictor for the next pixel
-            predictor += quantized_error
-
-            # Reconstruct the pixel value and clip it to the valid range [0, 255]
-            decoded[row, col] = np.clip(predictor, 0, 255)
-
-    return decoded
+    compressed_image.save(f'images/compressed-{compression_strength}-cat.jpg')
 
 
 image_path = 'images/cat.jpg'
-original_image = Image.open(image_path).convert('L')  # Convert to grayscale
+# open image as grayscale
+original_image = Image.open(image_path).convert('L')
 original_array = np.array(original_image)
 
-# Encode the image using DPCM
-encoded_values = dpcm_encode(original_array)
+# define the compression strengths
+compression_strengths = [1, 2, 4, 8, 16]
+for comperssion in compression_strengths:
+    encoded_values = dpcm_encode(original_array, comperssion)
 
-# Decode the encoded values
-decoded_image_array = dpcm_decode(encoded_values, original_array.shape)
-
-# Convert NumPy arrays to PIL Images
-original_pil_image = Image.fromarray(original_array)
-decoded_pil_image = Image.fromarray(decoded_image_array)
-
-decoded_pil_image.save('images/decoded_image.jpg')
+    save_as_compressed_image(encoded_values, original_array.shape, comperssion)
